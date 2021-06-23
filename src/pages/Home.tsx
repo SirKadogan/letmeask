63922b3b59a8a9031fdc684150aee1c26281d748
Thales Kenne <thales.kenne@altimus.com.br>
2021-06-23 19:12:17 -0300

    Styling login screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,30 +2,33 @@ import IllustrationImg from '../assets/images/illustration.svg';
 import LogoImg from '../assets/images/logo.svg';
 import GoogleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
 export default function Home() {
   return (
-    <div>
+    <div id ="page-auth">
       <aside>
         <img src={IllustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
         <strong>{"Crie sala de Q&A ao vivo"}</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={LogoImg} alt="Letmeask"/>
-          <button>
+          <button className="create-room">
             <img src={GoogleIconImg} alt="Logo do google"/>
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input
             type = "text"
             placeholder = "Digite o código da sala"
             />
-            <button type="submit">
+            <Button type="submit">
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>
