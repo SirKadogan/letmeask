c2f9220ccf739568107868587607c4c844d88cd9
Thales Kenne <thales.kenne@altimus.com.br>
2021-06-25 14:43:53 -0300

    Adding hook to reuse logic of fetching questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -8,61 +8,21 @@ import { FormEvent, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 import { useEffect } from 'react';
+import Question from '../components/Question';
+import { useRoom } from '../hooks/useRoom';
 
 type RoomParams = {
   id: string;
 }
 
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string;
-    avatar: string;
-  },
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-}>; 
 
-type Question = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  },
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-}
 
 export default function Room() {
   const {user} = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [title, setTitle] = useState('');
-
-
-  useEffect(() => {
-    const roomRef = database.ref(`/rooms/${roomId}`);
-
-    roomRef.on('value', room => {
-      const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-      
-      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-        return {
-          id: key,
-          content: value.content,
-          author: value.author,
-          isAnswered: value.isAnswered,
-          isHighlighted: value.isHighlighted,
-        };
-      });
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions);
-    })
-  }, [roomId])
+  const {questions, title} = useRoom(roomId);
 
   const handleSendQuestion = async (event: FormEvent) => {
     event.preventDefault();
@@ -123,7 +83,9 @@ export default function Room() {
           <Button disabled={!user}>Enviar pergunta</Button>
         </div>
       </form>
-
+      <div className="question-list">
+        {questions.map((question) => <Question key={question.id} {...question}/>)}
+      </div>
     </main>
   </div>
 }
