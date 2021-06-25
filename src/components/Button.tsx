35c2a8e81bc237fc19ded895596bf3b5d0c5b872
Thales Kenne <thales.kenne@altimus.com.br>
2021-06-25 14:57:15 -0300

    Adding admin room

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,14 @@ import {ButtonHTMLAttributes} from 'react';
 
 import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
 
 export function Button(props:ButtonProps) {
+  const { isOutlined = false } = props;
+
   return (
-    <button className="button" {...props}/>
+    <button className={`button ${isOutlined ? 'outlined' : ''}`} {...props}/>
   )
 }
